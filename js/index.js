2bd15db28331d16ab1bfc036642bf8178eae9e41
daniya-pro <syedadaniyahaider@gmail.com>
2021-01-08 21:48:46 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -35,5 +35,55 @@ err404.style.height=bodyel.offsetHeight
 err404.style.width=bodyel.offsetWidth
 
 }
+getData()
+window.onresize = ()=>{resizingImg()  }
+resizingImg()
 
+
+
+
+
+
+var setHtml=(data)=>{
+var list=  document.getElementById("list")
+list.innerHTML=''
+console.log(1,data,'d.v',data.val())
+
+
+}}else{
+  
+var pushData = () => {
+  var flag = ''
+
+document.querySelectorAll("input").forEach((a) => {
+  if (a.value.trim() === '') {
+    flag = "falses"
+
+    $('.ui.modal')
+      .modal('show');
+  } else {
+    if (flag === 'falses') { } else {
+alert()
+      flag = 'true'
+    }
+  }
+
+
+})
+if (flag === 'true') {
+  firebase.database().ref('prodectDeteils').push({
+    productName: document.getElementById('productName').value,
+    productPrice: document.getElementById('productPrice').value,
+    productImagelink: document.getElementById('productImage').value,
+    productDescription: document.getElementById('productDescription').value
+    
+
+  })
+  location.href='/pages'
+}
+
+}
+
+var btn=document.getElementById('SubmitBtn')
+btn.addEventListener('click', pushData)
 }
